0f87f1b7851dacf447393dad4f53a7ec68632d40
Salimov-dev <ruspb1987@gmail.com>
2024-07-25 15:43:41 +0300

    проект инициализирован, компонент Header создан

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import { Title, Container } from "@/components/shared";
+
+export default function Home() {
+  return (
+    <>
+      <Container>
+        <Title />
+      </Container>
+    </>
+  );
+}
